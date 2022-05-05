e49b90e0ea8fe6f104f01d06b7cc1a865dc0874c
jackra1n <lok7ok@gmail.com>
2022-05-05 17:14:25 +0200

    Use one button instead of 2. Improve style

--- sidepanel-hider.user.js
+++ sidepanel-hider.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jenkins Pipeline Sidepanel Hider
 // @namespace    https://github.com/jackra1n
-// @version      1.2
+// @version      1.3
 // @description  Adds new buttons that allow you to hide the side panel on jenkins job pipelines
 // @author       jackra1n
 // @include      /^(http[s]?):\/\/.*jenkins.*$/
@@ -9,87 +9,93 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // ==/UserScript==
-(function() {
+(function () {
 
-    GM_config.init(
+  let isSidePanelVisible = true;
+
+  GM_config.init(
     {
       'id': 'MyConfig', // The id used for this instance of GM_config
       'fields': // Fields object
+      {
+        'sidepanelWidth': // This is the id of the field
         {
-            'sidepanelWidth': // This is the id of the field
-            {
-                'label': 'side-panel width', // Appears next to field
-                'type': 'int', // Makes this setting a text field
-                'default': '310' // Default value if user doesn't change it
-            }
+          'label': 'side-panel width', // Appears next to field
+          'type': 'int', // Makes this setting a text field
+          'default': '310' // Default value if user doesn't change it
         }
+      }
     });
 
-    //Add material icons for button icons
-    let head = document.getElementsByTagName("head")[0];
-    let link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
-    head.appendChild(link);
+  //Add material icons for button icons
+  let head = document.getElementsByTagName("head")[0];
+  let link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+  head.appendChild(link);
 
-    let sidePanel = $("side-panel");
-    let mainPanel = $("main-panel");
-    let header = $("header");
+  let sidePanel = $("side-panel");
+  let mainPanel = $("main-panel");
+  let header = $("header");
 
-    let buttonSettings = createButton("settings");
-    buttonSettings.style.padding = "6px";
-    
-    let buttonShow = createButton("keyboard_arrow_right");
-    buttonShow.style.display = "none";
+  let settingsBtn = createButton("settings");
+  settingsBtn.style.padding = "6px";
+  settingsBtn.style.border = "none";
 
-    let buttonHide = createButton("keyboard_arrow_left");
-    buttonHide.style.float = "right";
-    buttonHide.style.marginRight = "15px";
-        
-    let showButtonDiv = document.createElement("div");
-    showButtonDiv.style.height = '28px';
+  let sidepanelBtn = createButton("keyboard_arrow_left");
+  sidepanelBtn.style.border = '2px solid var(--task-link-bg-color--active)';
+  sidepanelBtn.style.borderRadius = '0px 6px 6px 0px';
+  sidepanelBtn.style.marginLeft = '-16px';
+  sidepanelBtn.style.marginTop = '2px';
+  sidepanelBtn.style.borderLeft = 'none';
+  sidepanelBtn.style.width = '66px';
 
-    header.insertBefore(buttonSettings, header.childNodes[2]);
-    sidePanel.insertBefore(buttonHide, sidePanel.childNodes[0]);
-    mainPanel.insertBefore(showButtonDiv, mainPanel.childNodes[0]);
-    showButtonDiv.insertBefore(buttonShow, showButtonDiv.childNodes[0]);
+  let buttonDiv = document.createElement("div");
 
-    sidePanel.style.overflowX = "hidden";
-    sidePanel.style.transition = "0.5s";
-    mainPanel.style.padding = '1rem';
-    mainPanel.style.transition = "margin-left .5s";
+  header.insertBefore(settingsBtn, header.childNodes[2]);
+  mainPanel.insertBefore(buttonDiv, mainPanel.childNodes[0]);
+  buttonDiv.insertBefore(sidepanelBtn, buttonDiv.childNodes[0]);
 
-    buttonHide.addEventListener("click", ()=>{
-        sidePanel.style.width = "0";
-        buttonShow.style.display = "flex";
-        window.localStorage.setItem("isSidepanelClosed", "true");
-    });
+  sidePanel.style.overflowX = "hidden";
+  sidePanel.style.transition = "0.5s";
+  mainPanel.style.padding = '1rem';
+  mainPanel.style.transition = "margin-left .5s";
 
-    buttonShow.addEventListener("click", ()=>{
-        sidePanel.style.width = GM_config.get('sidepanelWidth')+"px";
-        buttonShow.style.display = "none";
-        window.localStorage.setItem("isSidepanelClosed", "false");
-    });
-
-    if(window.localStorage.getItem("isSidepanelClosed") == "true")
-    {
-        buttonShow.style.display = "flex";
-        sidePanel.style.width = "0";
+  sidepanelBtn.addEventListener("click", () => {
+    if (window.localStorage.getItem("isSidepanelClosed") == "false") {
+      sidePanel.style.width = "0";
+      window.localStorage.setItem("isSidepanelClosed", "true");
+      sidepanelBtn.innerHTML = '<i class="material-icons">keyboard_arrow_right</i>';
     }
-
-    buttonSettings.addEventListener("click", ()=> {
-        GM_config.open();
-    });
-
-    function createButton(iconName) {
-        let btn = document.createElement("button");
-        btn.style.display = 'flex';
-        btn.style.padding = "0";
-        btn.style.borderRadius = '6px';
-        btn.style.backgroundColor = 'var(--background)';
-        btn.style.color = 'var(--text-color)';
-        btn.innerHTML = '<i class="material-icons">'+iconName+'</i>';
-        return btn;
+    else {
+      sidePanel.style.width = GM_config.get('sidepanelWidth') + "px";
+      window.localStorage.setItem("isSidepanelClosed", "false");
+      sidepanelBtn.innerHTML = '<i class="material-icons">keyboard_arrow_left</i>';
     }
+  });
+
+  if (window.localStorage.getItem("isSidepanelClosed") == "true") {
+    sidepanelBtn.innerHTML = '<i class="material-icons">keyboard_arrow_right</i>';
+    sidePanel.style.width = "0";
+  }
+
+  settingsBtn.addEventListener("click", () => {
+    GM_config.open();
+  });
+
+  function createButton(iconName) {
+    let btn = document.createElement("button");
+    btn.style.display = 'flex';
+    btn.style.justifyContent = 'center';
+    btn.style.alignItems = 'center';
+    btn.style.height = "40px";
+    btn.style.width = "40px";
+    btn.style.padding = "0";
+    btn.style.borderRadius = '6px';
+    btn.style.backgroundColor = 'var(--background)';
+    btn.style.color = 'var(--text-color)';
+    btn.innerHTML = '<i class="material-icons">' + iconName + '</i>';
+    return btn;
+  }
 })();
